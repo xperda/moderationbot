dad38832341363b23ba73686a51e013432847c80
desxeda <desxeda@gmail.com>
2018-10-31 14:00:32 +0800

    add simple word filter

--- cogs/filter.py
+++ cogs/filter.py
@@ -1,9 +1,21 @@
+import json
 from discord.ext import commands
 
+
 class Filter:
+
+    with open('banwords.json') as ban:
+        banned = json.load(ban)
+
     def __init__(self,bot):
         self.bot = bot
-
+    #word filter
+        @bot.event
+        async def on_message(message):
+            if any(word in message.content.lower() for word in banned):
+                await bot.delete.message(message)
+                mention = '{0.author.mention}'.format(message)
+                await bot.send_message(message.channel,mention)
 
 def setup(bot):
     bot.add_cog(Filter(bot))
