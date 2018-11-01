6ef165a7dd9ce660c7f588b36c07a7980eaadcea
desxeda <desxeda@gmail.com>
2018-11-02 04:12:29 +0800

    added func to main

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import discord
 import sqlite3
-from cogs import filter
-
 
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="?",description="test")
+#util imports
+from utils.config import ConfigLoader
+from utils.database import DatabaseHandler
+
+PREFIX = ConfigLoader.load_config_setting('Bot Settings','command_prefix')
+DESC = ConfigLoader.load_config_setting('Bot Settings','description')
+TOKEN = ConfigLoader.load_config_setting('Bot Settings','bot_token')
+DATABASE = ConfigLoader.load_config_setting('Bot Settings','database')
+
+bot = commands.Bot(command_prefix=PREFIX,description=DESC)
+
 bot.remove_command('help')
 
 cogs = ['cogs.basic',
@@ -30,4 +38,4 @@ if __name__ == '__main__':
 
 
 
-bot.run('NTA2MzMyMzc3MTcwMDUxMTAz.DrqscA.hZqpb2KiuB3YSLoiCUhvl_-G3h4')
+bot.run(TOKEN)
