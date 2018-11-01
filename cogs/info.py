88cac15ce084d9e405a3a4a40a66509caf639f67
desxeda <desxeda@gmail.com>
2018-11-02 01:55:25 +0800

    Basic help command structure added

--- cogs/info.py
+++ cogs/info.py
@@ -2,8 +2,22 @@ import discord
 from discord.ext import commands
 
 class Info:
+
+
+
     def __init__(self,bot):
         self.bot = bot
+
+
+    @commands.command(pass_context=True)
+    async def help(self,ctx):
+        embed = discord.Embed(
+            color=discord.Colour.red()
+        )
+
+        embed.add_field(name="help", value="ping - returns a pong")
+
+        await self.bot.send_message(ctx.message.channel,embed=embed)
 
 
 def setup(bot):
