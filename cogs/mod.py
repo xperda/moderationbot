0b2194d3a1c991d175993fe116c48c8239bb7ba0
desxeda <desxeda@gmail.com>
2018-11-11 21:12:55 +0800

    Fixed mod commands

--- cogs/mod.py
+++ cogs/mod.py
@@ -11,11 +11,10 @@ class Moderation:
         self.bot = bot
 
 
+
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True,kick_members=True)
     async def kick(self ,ctx,user:discord.Member,reason=""):
-
-        author = ctx.message.author
 
         try:
             if reason == "":
@@ -23,10 +22,10 @@ class Moderation:
             else:
                 msg = "You have been kicked from the server for {}".format("reason")
             msg += "\nYou can rejoin the server, but please read and respect the rules of the server before rejoining."
+            await self.bot.send_message(user, msg)
             await self.bot.kick(user)
-            await self.bot.send_message(author, msg)
-        except discord.errors.Forbidden:
-            await self.bot.say("I don't have the permission to do this command")
+            await self.bot.say("{} has been kicked from the server".format(user.name))
+
         except CheckFailure:
             await self.bot.say("You aren't qualified to use this command")
 
@@ -34,28 +33,27 @@ class Moderation:
     @commands.has_permissions(administrator=True, ban_members=True)
     async def ban(self, ctx, user: discord.Member, reason=""):
 
-        author = ctx.message.author
 
         try:
             if reason == "":
                     msg = "You have been banned from the server"
             else:
                     msg = "You have been banned from the server for {}".format("reason")
+            await self.bot.send_message(user, msg)
             await self.bot.ban(user)
-            await self.bot.send_message(author, msg)
-        except discord.errors.Forbidden:
-            await self.bot.say("I don't have the permission to do this command")
+            await self.bot.say("{} has been banned from the server".format(user.name))
+
         except CheckFailure:
             await self.bot.say("You aren't qualified to use this command")
 
-    @commands.command(name="clear", pass_context=True)
-    async def clear(self, ctx, amount):
-        channel = ctx.message.channel
-        messages = []
-        async for msg in self.bot.logs_from(channel, limit=int(amount)):
-            messages.append(msg)
-        await asyncio.sleep(1)
-        await self.bot.delete_messages(messages)
+
+    @commands.command(pass_context=True)
+    async def banlist(self,ctx):
+        server = ctx.message.author.server
+        server_id = server.id
+        banendUsers = await self.bot.get_bans(server_id)
+        for user in banendUsers:
+            await self.bot.say(user.name+":"+user.id)
 
 
     #profanity filter
@@ -65,7 +63,7 @@ class Moderation:
             if word.upper() in chat_filter:
                 try:
                     await self.bot.delete_message(message)
-                    await self.bot.say("You aren't allowed to say that in here!")
+                    await self.bot.send_message("You aren't allowed to say that in here!")
                 except discord.errors.NotFound:
                     return
 
